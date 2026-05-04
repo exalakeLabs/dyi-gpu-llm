4a896e29aea7d92fe0df4c6da418c4042d1767e7
Alexander Gauthier <github@exalake.com>
2026-05-03 21:52:28 -0700

    Add embed-model option to build_rag_index.zsh

--- build_rag_index.zsh
+++ build_rag_index.zsh
@@ -1,6 +1,7 @@
 python ./src/index_builder.py \
   --input-dir $LLAMA_PREPARED_DIR \
   --output-dir $LLAMA_RAG_DIR \
+  --embed-model BAAI/bge-m3 \
   --chunk-size-chars 1800 \
   --overlap-chars 250 \
   --batch-size 32
