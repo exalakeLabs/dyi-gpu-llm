bb5df25b6cd49895677e8f77b1a445c03053a27d
Alexander Gauthier <github@exalake.com>
2026-05-03 18:54:54 -0700

    wefs

--- build_rag_index.zsh
+++ build_rag_index.zsh
@@ -1,11 +1,6 @@
 python ./src/index_builder.py \
-
   --input-dir /datasets/llama/text \
-
   --output-dir ~/llrun/gutenberg_bge_index \
-
   --chunk-size-chars 1800 \
-
   --overlap-chars 250 \
-
   --batch-size 32
