79eb3143c3f108ea65631c0e21e186fb4dfda347
Alexander Gauthier <github@exalake.com>
2026-05-03 18:44:29 -0700

    Add build script for RAG index generation

---
+++ build_rag_index.zsh
@@ -0,0 +1,11 @@
+python index_builder.py \
+
+  --input-dir /datasets/llama/text \
+
+  --output-dir ~/llrun/gutenberg_bge_index \
+
+  --chunk-size-chars 1800 \
+
+  --overlap-chars 250 \
+
+  --batch-size 32
